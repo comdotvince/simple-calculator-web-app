aaea3b03d5217e85e433b69ce3f69e468ea7299a
Vince Ferreras Ermino <vinceferrerasermino25@gmail.com>
2025-05-08 08:47:30 +0800

    update logic

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
+let expression = "";
+let resultShown = false;
+
+const input = document.querySelector("#input-js");
 const buttons = document.querySelectorAll(".btn-js");
 
-let input = document.querySelector("#input-js");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const val = btn.innerHTML;
 
-let inputValue = input.value;
-
-let expression = "";
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const inputBtn = button.innerHTML;
-
-    expression += inputBtn;
-    input.value = expression;
-
-    console.log(expression);
-
-    if (inputBtn === "=") {
-      inputValue = math.evaluate(expression.slice(0, -1));
-      console.log(inputValue);
-      input.value = inputValue;
+    if (val === "AC") {
       expression = "";
-    }
-
-    if (inputBtn === "AC") {
-      expression = "";
-      input.value = "";
+      input.value = "0";
+      resultShown = false;
+    } else if (val === "=") {
+      try {
+        expression = math.evaluate(expression).toString();
+        input.value = expression;
+        resultShown = true;
+      } catch {
+        input.value = "Error";
+        expression = "";
+      }
+    } else if (val === "+/-") {
+      // Flip the sign of last number
+      const match = expression.match(/(-?\d+\.?\d*)$/);
+      if (match) {
+        const num = match[0];
+        const flipped = (-parseFloat(num)).toString();
+        expression = expression.slice(0, -num.length) + flipped;
+        input.value = expression;
+      }
+    } else {
+      // If result was just shown, start new expression
+      if (resultShown && /[0-9.]/.test(val)) {
+        expression = val;
+      } else {
+        expression += val;
+      }
+      input.value = expression;
+      resultShown = false;
     }
   });
 });
